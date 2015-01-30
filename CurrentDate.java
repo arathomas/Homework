8e90888cd0da73347dce777abddb7d99b68e12e0
arathomas <arathomas3@gmail.com>
2015-01-30 14:32:25 +0100

    first program

---
+++ CurrentDate.java
@@ -0,0 +1,15 @@
+package date;
+
+import java.time.LocalDateTime;
+
+public class CurrentDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		LocalDateTime date = LocalDateTime.now();
+		System.out.println(date);
+
+	}
+
+}
